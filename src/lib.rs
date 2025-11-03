ce0e9b0da7608ad846a8d70d0ee597adbeb258bd
Marcos Brito <marcos.brito0507@gmail.com>
2025-11-03 10:56:33 -0300

    feat: create Site struct to hold results

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,31 @@ pub mod query;
 pub mod matcher;
 
 #[derive(PartialEq, Debug)]
+pub struct Site {
+    domain: String,
+    logo: Option<Logo>,
+    favicon: Option<Logo>,
+}
+
+impl Site {
+    pub fn new(domain: String) -> Self {
+        Self {
+            domain,
+            logo: None,
+            favicon: None,
+        }
+    }
+
+    pub fn to_csv(&self) -> String {
+        format!(
+            "{}, {}, {}",
+            &self.domain,
+            &self.logo.as_ref().map_or("null", |l| &l.url),
+            &self.favicon.as_ref().map_or("null", |l| &l.url),
+        )
+    }
+}
+
 pub struct Logo {
     url: String,
     mime: Option<String>,
